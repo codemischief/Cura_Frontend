d6e3cb95cb8273b4b9f9d2b7a976eeeae94acf69
Zamster44 <zameersid10@gmail.com>
2024-02-26 16:55:11 +0530

    updated manage page and login

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -12,91 +12,93 @@ const login = () => {
   const database = [
     {
       username: "user1",
-      password: "pass1"
+      password: "pass1",
     },
     {
       username: "user2",
-      password: "pass2"
-    }
+      password: "pass2",
+    },
   ];
 
   const initialValues = {
-    username:"",
-    password:"",
-    comkey:""
+    username: "",
+    password: "",
+    comkey: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [type, setType] = useState("password");
 
-  }
-  const [formValues, setFormValues]= useState(initialValues);
-  const [formErrors, setFormErrors]= useState({});
-  const [isSubmit, setIsSubmit]= useState(false);
-  const [type, setType] = useState('password');
- 
+  // handle changes in input form
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-// handle changes in input form
-  const handleChange = (e) =>{
-    const {name, value} =e.target;
-    setFormValues({...formValues, [name]:value})   
-  }
-
-// password visibility
+  // password visibility
   const passwordToggle = () => {
-      if (type==='password'){
-        setType('text')
-     } else {
-        setType('password')
-     }
- }
-    
-  
- const comkeyToggle = () =>{
-        if (type==='password'){
-             setType('text')
-           } else {
-              setType('password')
-          }
+    if (type === "password") {
+      setType("text");
+    } else {
+      setType("password");
     }
-    
+  };
 
-    
-const handleSubmit = (e) =>{
-  e.preventDefault();        
-  setFormErrors(validate(formValues));  // validate form and set error message
-  setIsSubmit(true);
-}
+  const comkeyToggle = () => {
+    if (type === "password") {
+      setType("text");
+    } else {
+      setType("password");
+    }
+  };
 
-// validate form and to throw Error message
-const validate = (values) =>{
-  const errors= {};
-  if(!values.username){
-    errors.username= "username is required!"
+  const changeStyle = () =>{
+    const element = document.getElementById("bottom-loginbtn");
+    element.classList.remove("loginBtn");  
+    element.classList.add("modified-loginBtn");
   }
-  if(!values.password){
-    errors.password= "password is required!"
-  }
-  if(!values.comkey){
-    errors.comkey= "company key is required!"
-  }
-  return errors;
-}
 
-useEffect(() =>{
-  if(Object.keys(formErrors).length == 0 && isSubmit){
-     // Find user login info
-        const userData = database.find((user) => user.username === formValues.username);
-        if(userData){
-          if (userData.password !== formValues.password) {
-            // Invalid password
-            console.log("invalid password")
-          } else {
-            navigate("/dashboard");
-            console.log("login successful")
-          }
-        }else{
-          console.log("user not found")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues)); // validate form and set error message
+    setIsSubmit(true);
+  };
+
+  // validate form and to throw Error message
+  const validate = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = "username is required!";
+    }
+    if (!values.password) {
+      errors.password = "password is required!";
+    }
+    if (!values.comkey) {
+      errors.comkey = "company key is required!";
+    }
+    return errors;
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length == 0 && isSubmit) {
+      // Find user login info
+      const userData = database.find(
+        (user) => user.username === formValues.username
+      );
+      if (userData) {
+        if (userData.password !== formValues.password) {
+          // Invalid password
+          console.log("invalid password");
+        } else {
+          navigate("/dashboard");
+          console.log("login successful");
         }
-  }
-},[formErrors]
-)
+      } else {
+        console.log("user not found");
+      }
+    }
+  }, [formErrors]);
 
   return (
     <div className="login">
@@ -106,39 +108,67 @@ useEffect(() =>{
       <div className="main">
         <div className="upper">
           <div className="heading">Welcome Back!</div>
-          <form  onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className="username">
               <div className="form-heading">Username</div>
-              <input className="form-input" name="username" type="text" value={formValues.username} onChange={handleChange} autoComplete="off" />
+              <input
+                className="form-input"
+                name="username"
+                type="text"
+                value={formValues.username}
+                onChange={handleChange}
+                autoComplete="off"
+              />
               <span className="error">{formErrors.username}</span>
             </div>
             <div className="password">
               <div className="form-heading">Password</div>
-              <div className="input_group" > 
-              <input className="form-input" name="password" type={type} value={formValues.password} onChange={handleChange} autoComplete="off"/>
-              <span className="eye_icon"><img className="span" onClick={passwordToggle} src={eyeIcon} alt="eye-icon"/></span>
+              <div className="input_group">
+                <input
+                  className="form-input"
+                  name="password"
+                  type={type}
+                  value={formValues.password}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <span className="eye_icon">
+                  <img
+                    className="span"
+                    onClick={passwordToggle}
+                    src={eyeIcon}
+                    alt="eye-icon"
+                  />
+                </span>
               </div>
               <span className="error">{formErrors.password}</span>
-              
             </div>
             <div className="company-key">
               <div className="form-heading">Company Key</div>
-              <div className="input_group" > 
-              <input className="form-input" name="comkey"  type={type} value={formValues.comkey} onChange={handleChange} autoComplete="off"/>
-              <span className="eye_icon"><img  onClick={comkeyToggle} src={eyeIcon} alt="eye-icon"/></span>
+              <div className="input_group">
+                <input
+                  className="form-input"
+                  name="comkey"
+                  type={type}
+                  value={formValues.comkey}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <span className="eye_icon">
+                  <img onClick={comkeyToggle} src={eyeIcon} alt="eye-icon" />
+                </span>
               </div>
               <span className="error">{formErrors.comkey}</span>
-              
             </div>
             <div className="bottom">
-                <a className="forget-link" href="#">
-                    Forgot your password
-                 </a>
-                <button className="loginBtn">Login</button>
-             </div>
-            
+              <a className="forget-link" href="#">
+                Forgot your password
+              </a>
+              <button className="loginBtn" id="bottom-loginbtn" onClick={changeStyle}>
+                Login
+              </button>
+            </div>
           </form>
-
         </div>
 
         {/* <div className="bottom">
@@ -148,7 +178,6 @@ useEffect(() =>{
           <button className="loginBtn">Login</button>
         </div> */}
       </div>
-     
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,29 +4,38 @@ import LogoutIcon from './assets/logout.png'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const display = () => {
+    let element=document.getElementById("dashboard");
+    // if(element.style.display=="hidden"){
+      element.style.display="inline-block";
+    // }
+    // else{
+    //   element.style.display="hidden";
+    // }
+  }
   return (
     <>
       <div className="bg-[#004DD7] h-[65px] flex items-center space-x-[240px]">
         <div className=" ml-[35px] flex space-x-[50px]">
           <img className="w-[68px]" src={logo} alt="company logo" />
           <div className="text-white space-x-[40px]">
-            <a href="#">Admin</a>
-            <Link to="/manageuser">Manage</Link>
-            <a href="#">task</a>
-            <a href="#">Report</a>
-            <a href="#">Research</a>
+            <button className=""><Link to="#">Admin</Link></button>
+            <button><Link to="/manageuser">Manage</Link></button>
+            <button><Link to="#">task</Link></button>
+            <button><Link to="#">Report</Link></button>
+            <button><Link to="#">Research</Link></button>
           </div>
         </div>
         <div className="flex items-center text-white space-x-[50px]">
-          <a href="#">DashBoard</a>
-          <a href="#">Change Password</a>
+          <button className="" onClick={display} ><Link to="/dashboard">DashBoard</Link></button>
+          <button><Link to="#">Change Password</Link></button>
           <div className="flex items-center space-x-2">
             <img className="w-[18xp] h-[18px] " src={LogoutIcon} alt="logout-icon" />
-            <a href="#">Logout</a>
+            <Link to="/">Logout</Link>
           </div>
         </div>
       </div>
-      <div className="w-[1200px] h-[300] absolute top-[65px] left-[35px] bg-white rounded-xl px-[25px] py-[15px] drop-shadow-md flex">
+      <div className="w-[1200px] h-[300] absolute top-[65px] left-[35px] bg-white rounded-xl px-[25px] py-[15px] drop-shadow-md flex hidden" id="dashboard">
         <div className="w-[240px] space-y-[15px] mr-[45px]">
           <div className="text-[24px] ">Home</div>
           <div>DashBoard of all</div>
