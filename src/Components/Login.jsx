7d7b4016912a9f026108e331df47a48aab72660c
Zamster44 <zameersid10@gmail.com>
2024-02-28 14:03:31 +0530

    update login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Logo from "./assets/logo.jpg";
 import eyeIcon from "./assets/eye.jpg";
 import { useState, useEffect, useContext } from "react";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   // Testing User Login info
   const database = [
@@ -27,7 +27,9 @@ const Login = () => {
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
-  const [type, setType] = useState("password");
+  const [type1, setType1] = useState("password");
+  const [type2, setType2] = useState("password");
+
 
   // handle changes in input form
   const handleChange = (e) => {
@@ -37,25 +39,24 @@ const Login = () => {
 
   // password visibility
   const passwordToggle = () => {
-    if (type === "password") {
-      setType("text");
+    if (type1 === "password") {
+      setType1("text");
     } else {
-      setType("password");
+      setType1("password");
     }
   };
 
   const comkeyToggle = () => {
-    if (type === "password") {
-      setType("text");
+    if (type2 === "password") {
+      setType2("text");
     } else {
-      setType("password");
+      setType2("password");
     }
   };
 
-  const changeStyle = () =>{
-    const element = document.getElementById("bottom-loginbtn");
-    element.classList.remove("loginBtn");  
-    element.classList.add("modified-loginBtn");
+  const show = () => {
+    const element = document.getElementById("inputError");
+    element.style.display="block";
   }
 
   const handleSubmit = (e) => {
@@ -89,12 +90,14 @@ const Login = () => {
         if (userData.password !== formValues.password) {
           // Invalid password
           console.log("invalid password");
+          show();
         } else {
           navigate("/dashboard");
           console.log("login successful");
         }
       } else {
         console.log("user not found");
+        show();
       }
     }
   }, [formErrors]);
@@ -124,51 +127,57 @@ const Login = () => {
               </div>
               <div className="space-y-[2px]">
                 <div className="text-[#505050] text-[18px] ">Password</div>
-                <input
-                  className="border-[1px] border-[#C6C6C6] w-[400px] h-[30px] text-[#505050]"
-                  name="password"
-                  type={type}
-                  value={formValues.password}
-                  onChange={handleChange}
-                  autoComplete="off"
-                />
-                <span className="w-[20px] h-[20px] absolute right-[438px] bottom-[316px]">
-                  <img
-                    className='cursor-pointer'
-                    onClick={passwordToggle}
-                    src={eyeIcon}
-                    alt="eye-icon"
+                <div className="m-[0px] p-[0px] relative">
+                  <input
+                    className="border-[1px] border-[#C6C6C6] w-[400px] h-[30px] text-[#505050]"
+                    name="password"
+                    type={type1}
+                    value={formValues.password}
+                    onChange={handleChange}
+                    autoComplete="off"
                   />
-                </span>
-                <div className="text-[10px] text-[#CD0000] invisible">{formErrors.password}</div>
+                  <span className="w-[20px] h-[20px] absolute right-[10px] bottom-[3px]">
+                    <img
+                      className='cursor-pointer'
+                      onClick={passwordToggle}
+                      src={eyeIcon}
+                      alt="eye-icon"
+                    />
+                  </span>
+                </div>
+
+                <div className="text-[12px] text-[#CD0000] ">{formErrors.password}</div>
               </div>
               <div className="space-y-[2px]">
                 <div className="text-[#505050] text-[18px]">Company Key</div>
-                <input
-                  className="border-[1px] border-[#C6C6C6] w-[400px] h-[30px] text-[#505050]"
-                  name="comkey"
-                  type={type}
-                  value={formValues.comkey}
-                  onChange={handleChange}
-                  autoComplete="off"
-                />
-                <span className="w-[20px] h-[20px] absolute right-[438px] bottom-[228px]">
-                  <img
-                    className='cursor-pointer'
-                    onClick={passwordToggle}
-                    src={eyeIcon}
-                    alt="eye-icon"
+                <div className="m-[0px] p-[0px] relative">
+                  <input
+                    className="border-[1px] border-[#C6C6C6] w-[400px] h-[30px] text-[#505050]"
+                    name="comkey"
+                    type={type2}
+                    value={formValues.comkey}
+                    onChange={handleChange}
+                    autoComplete="off"
                   />
-                </span>
-                <div className="text-[10px] text-[#CD0000] invisible">{formErrors.comkey}</div>
+                  <span className="w-[20px] h-[20px] absolute right-[10px] bottom-[3px]">
+                    <img
+                      className='cursor-pointer'
+                      onClick={comkeyToggle}
+                      src={eyeIcon}
+                      alt="eye-icon"
+                    />
+                  </span>
+                </div>
+
+                <div className="text-[12px] text-[#CD0000] ">{formErrors.comkey}</div>
               </div>
             </div>
-            <div className="w-[450px] h-[74px] bg-[#FFEAEA] rounded-[15px] border-[1px] border-[#CD0000] flex justify-center items-center px-[45px] py-[20px] text-[14px] invisible">
-            Incorrect username,password or Company Key. Please enter the Correct Information and try again.
+            <div id="inputError" className="w-[450px] h-[74px] bg-[#FFEAEA] rounded-[15px] border-[1px] border-[#CD0000] flex justify-center items-center px-[45px] py-[20px] text-[14px] invisible">
+              Incorrect username,password or Company Key. Please enter the Correct Information and try again.
             </div>
             <div className="flex flex-col items-center justify-center gap-[10px]">
-                <Link className="text-[#004DD7] text-[18px] cursor-pointer" to="/">Forget your password?</Link>
-                <button className="bg-[#004DD7] w-[200px] h-[35px] text-white text-[18px] rounded-lg cursor-pointer">Login</button>
+              <Link className="text-[#004DD7] text-[18px] cursor-pointer" to="/">Forget your password?</Link>
+              <button className="bg-[#004DD7] w-[200px] h-[35px] text-white text-[18px] rounded-lg cursor-pointer">Login</button>
             </div>
           </form>
         </div>
