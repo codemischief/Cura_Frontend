88867644590e36aaae0ce75aefc57b445c1da55d
Abhishek Ghanekar <a8hiyt@gmail.com>
2024-03-04 19:45:05 +0530

    made the navbar responsive

--- src/Components/Navabar/Navbar.jsx
+++ src/Components/Navabar/Navbar.jsx
@@ -5,163 +5,154 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 const Navbar = () => {
  
-  const [isToggledDash, setIsToggled] = useState(false);
+  const [isToggledDash, setIsToggledDash] = useState(false);
   const [isToggledAdmin, setIsToggledAdmin] = useState(false);
-  const [isToggledManage, setIsToggledManage] = useState(false);
+  // const [isToggledManage, setIsToggledManage] = useState(false);
+ 
 
-
-  const handleChange = (e) => {
-    switch(e.target.outerText){
-      case 'DashBoard':
-        setIsToggled(!isToggledDash);
-        setIsToggledManage(false);
-        setIsToggledAdmin(false);
-        break;
-      case 'Admin':
-        setIsToggledAdmin(!isToggledAdmin);
-        setIsToggled(false);
-        setIsToggledManage(false);
-        break;
-      case 'Manage':
-        setIsToggledManage(!isToggledManage);
-        setIsToggled(false);
-        setIsToggledAdmin(false);
-        break;
-      default:
-        break;
-
-    }
-    
-   
+  const handleAdminChange = () => {
+      if(isToggledDash) {
+        handleDashChange();
+      }
+      setIsToggledAdmin((toggle) => {
+         return !toggle;
+      })
   };
-
+  const handleDashChange = () => {
+    if(isToggledAdmin) {
+      handleAdminChange();
+    }
+    setIsToggledDash((toggle) => {
+      return !toggle;
+    })
+  }
   return (
     <>
-      <div className="bg-[#004DD7] h-[65px] flex items-center space-x-[240px]">
-        <div className=" ml-[35px] flex space-x-[50px]">
-          <img className="w-[68px]" src={logo} alt="company logo" />
-          <div className="text-white space-x-[40px]">
-            <button className="" onClick={handleChange} id='admin'><Link to="">Admin</Link></button>
-            <button onClick={handleChange}><Link to="/manageuser">Manage</Link></button>
-            <button><Link to="#">task</Link></button>
-            <button><Link to="#">Report</Link></button>
-            <button><Link to="#">Research</Link></button>
-          </div>
+      <div className='bg-accent-blue w-screen  justify-between	 h-16 flex '>
+        {/* this div is off the navbar */}
+        {/* we have three parts here */}
+        <div className='h-full w-16 flex justify-center items-center'>
+           <img src={logo} className='h-1/3 ml-16'/>
         </div>
-        <div className="flex items-center text-white space-x-[50px]">
-          <button className="" onClick={handleChange} id="dashboard"><Link to="/dashboard">DashBoard</Link></button>
-          <button><Link to="#">Change Password</Link></button>
-          <div className="flex items-center space-x-2">
-            <img className="w-[18xp] h-[18px] " src={LogoutIcon} alt="logout-icon" />
-            <Link to="/">Logout</Link>
+        <div className='flex text-stone-50	space-x-9 items-center'>
+          {/* this will have the center bar */}
+          <div className='bg-white w-20 h-7 flex items-center justify-center text-accent-blue rounded-sm'>
+             <button onClick={handleAdminChange}>
+               <p className=' font-sans text-md' >Admin</p>
+             </button>
+             
+          </div>
+          <div>
+             <p className='font-thin font-sans text-md'>Manage</p>
+          </div>
+         
+          <p className='font-thin font-sans text-md'>Report</p>
+          <p className='font-thin font-sans text-md'>Research</p>
+        </div>
+        <div className='flex  text-stone-50	space-x-4 items-center mr-4'>
+          {/* this will have the right content */}
+          <button onClick={handleDashChange}>
+             <p className='font-thin font-sans text-md'>Dashboard</p>
+          </button>
+          
+          <p className='font-thin font-sans text-md'>Change Password</p>
+          <div className='flex just items-center '>
+             <img src={LogoutIcon} className='h-4 m-1' />
+             <p className='font-thin font-sans text-md'><Link to="/">Logout</Link> </p>
           </div>
         </div>
       </div>
+      {isToggledAdmin && <div className='bg-white rounded-lg mt-2 w-5/6 h-[370px] flex font-sans ml-5 absolute top-16'>
+         <div className=' w-1/4 h-full flex-col '>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Personal</h1>
+               <p className='text-thin text-base	'>Users</p>
+               <p className='text-thin text-base	'>Employees</p>
+               <p className='text-thin text-base'>Contractuel Payment</p>
 
-    {isToggledDash &&  <div className="w-[1200px] h-[300] absolute top-[65px] left-[35px] bg-white rounded-xl px-[25px] py-[15px] drop-shadow-md flex " id="dashboard">
-        <div className="w-[240px] space-y-[15px] mr-[45px]">
-          <div className="text-[24px] ">Home</div>
-          <div className='text-[14px]'>DashBoard of all</div>
-          <div className='text-[14px]'>Monthly Margin-LOB-Receipt-Payments</div>
-          <div className='text-[14px]'>Monthly Margin-Entity-Receipt-Payments</div>
-          <div className='text-[14px]'>Monthly Margin-LOB-Receipt-Payments Consolidated</div>
-        </div>
-        <div class="absolute left-[264px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Personnel</div>
-          <div className='text-[14px]'>Users</div>
-          <div className='text-[14px]'>Employees</div>
-          <div className='text-[14px]'>Contractual Payment</div>
-        </div>
-        <div class="absolute left-[485px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Offerrings</div>
-          <div className='text-[14px]'>LOB (Line of Business)</div>
-          <div className='text-[14px]'>Services</div>
-        </div>
-        <div class="absolute left-[700px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Locations</div>
-          <div className='text-[14px]'>Country</div>
-          <div className='text-[14px]'>State</div>
-          <div className='text-[14px]'>City</div>
-          <div className='text-[14px]'>Locality</div>
-        </div>
-        <div class="absolute left-[920px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Data management</div>
-          <div className='text-[14px]'>Delete by ID</div>
-        </div>
-      </div>}
+            </div>
+            <div className='ml-5 mt-9 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Margin Report</h1>
+               <p className='text-thin text-base'>LOB- Receipts - Payments</p>
+               <p className='text-thin text-base'>Entity-Receipts-Patments</p>
+               <p className='text-thin text-base'>LOB-Receipts-Payments</p>
+            </div>
+         </div>
+         <div class="  w-[2px] h-full bg-[#CBCBCB]"></div>
+         <div className=' w-1/4 h-full'>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Offerings </h1>
+               <p className='text-thin text-base'>LOB (Line of business)</p>
+               <p className='text-thin text-base'>Services</p>
+            </div>
+         </div>
+         <div class="  w-[2px] h-full bg-[#CBCBCB]"></div>
+         <div className=' w-1/4 h-full'>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Locations</h1>
+               
+               <p className='text-thin text-base'>Country</p>
+               <p className='text-thin text-base'>State</p>
+               <p className='text-thin text-base'>City</p>
 
-     {isToggledAdmin && <div className="w-[1200px] h-[300] absolute top-[65px] left-[35px] bg-white rounded-xl px-[25px] py-[15px] drop-shadow-md flex " id="admin">
-        <div className="w-[300px] space-y-[15px]">
-          <div className="text-[24px]">Personnel</div>
-          <div className='text-[14px]'>Users</div>
-          <div className='text-[14px]'>Employees</div>
-          <div className='text-[14px]'>Contractual Payment</div>
-          <div className="pt-[10px] w-[200px] space-y-[10px]">
-            <div className="text-[24px]">Margin report</div>
-            <div className='text-[14px]'>Monthly Margin-LOB-Receipt-Payments</div>
-            <div className='text-[14px]'>Monthly Margin-Entity-Receipt-Payments</div>
-            <div className='text-[14px]'>Monthly Margin-LOB-Receipt-Payments Consolidated</div>
-          </div>
-        </div>
-        <div class="absolute left-[264px] -ml-0.5 w-[2px] h-[350px] bg-[#CBCBCB]"></div>
-        <div className="w-[300px] space-y-[15px]">
-          <div className="text-[24px]">Offerrings</div>
-          <div className='text-[14px]'>LOB (Line of Business)</div>
-          <div className='text-[14px]'>Services</div>
-        </div>
-        <div class="absolute left-[525px] -ml-0.5 w-[2px] h-[350px] bg-[#CBCBCB]"></div>
-        <div className="w-[300px] space-y-[15px]">
-          <div className="text-[24px]">Locations</div>
-          <div className='text-[14px]'><Link to="/Country">Country</Link></div>
-          <div className='text-[14px]'><Link to="/State">State</Link></div>
-          <div className='text-[14px]'><Link to="/City">City</Link></div>
-          <div className='text-[14px]'><Link to="/Locality">Locality</Link></div>
-        </div>
-        <div class="absolute left-[810px] -ml-0.5 w-[2px] h-[350px] bg-[#CBCBCB]"></div>
-        <div className="w-[300px] space-y-[15px]">
-          <div className="text-[24px]">Data management</div>
-          <div className='text-[14px]'>Delete by ID</div>
-        </div>
+            </div>
+         </div>
+         <div class="  w-[2px] h-full bg-[#CBCBCB]"></div>
+         <div className=' w-1/4 h-full'>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Data Management</h1>
+               <p className='text-thin text-base'>Delete by ID</p>
+            </div>
+         </div>
       </div>}
-      {isToggledManage &&  <div className="w-[1200px] h-[300] absolute top-[65px] left-[35px] bg-white rounded-xl px-[25px] py-[15px] drop-shadow-md flex " id="dashboard">
-        <div className="w-[240px] space-y-[15px] mr-[45px]">
-          <div className="text-[24px] ">Home</div>
-          <div className='text-[14px]'>DashBoard of all</div>
-          <div className='text-[14px]'>Monthly Margin-LOB-Receipt-Payments</div>
-          <div className='text-[14px]'>Monthly Margin-Entity-Receipt-Payments</div>
-          <div className='text-[14px]'>Monthly Margin-LOB-Receipt-Payments Consolidated</div>
-        </div>
-        <div class="absolute left-[264px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Personnel</div>
-          <div className='text-[14px]'>Users</div>
-          <div className='text-[14px]'>Employees</div>
-          <div className='text-[14px]'>Contractual Payment</div>
-        </div>
-        <div class="absolute left-[485px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Offerrings</div>
-          <div className='text-[14px]'>LOB (Line of Business)</div>
-          <div className='text-[14px]'>Services</div>
-        </div>
-        <div class="absolute left-[700px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Locations</div>
-          <div className='text-[14px]'>Country</div>
-          <div className='text-[14px]'>State</div>
-          <div className='text-[14px]'>City</div>
-          <div className='text-[14px]'>Locality</div>
-        </div>
-        <div class="absolute left-[920px] -ml-0.5 w-[2px] h-[240px] bg-[#CBCBCB]"></div>
-        <div className="w-[240px] space-y-[15px]">
-          <div className="text-[24px]">Data management</div>
-          <div className='text-[14px]'>Delete by ID</div>
-        </div>
-      </div>}
+      {isToggledDash && <div className='bg-white rounded-lg mt-2 w-5/6 h-[370px] flex font-sans right-14 absolute top-16 scroll-mr-7'>
+         <div className=' w-1/5 h-full flex-col '>
+            <div className='ml-5 mt-4 flex-col space-y-2 w-3/4'>
+               <h1 className='font-semibold text-xl	'>Home </h1>
+               <p className='text-thin text-base'>Dashboard of all</p>
+               <p className='text-thin text-base'>Monthly Margins - LOB Receipts Payments</p>
+               <p className='text-thin text-base'>Monthly Margins - Entity Receipts-Paymen</p>
+               <p className='text-thin text-base'>Monthly Margins - LOB Receipts Payments Consolidated</p>
+            </div>
+         </div>
+         <div class="  w-[2px] h-full bg-[#CBCBCB]"></div>
+         <div className=' w-1/5 h-full'>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Personnel </h1>
+               <p className='text-thin text-base'>Users</p>
+               <p className='text-thin text-base'>Employees</p>
+               <p className='text-thin text-base'>Contractual Payment</p>
+            </div>
+         </div>
+         <div class="  w-[2px] h-full bg-[#CBCBCB]"></div>
+         <div className=' w-1/5 h-full'>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Offerings</h1>
+               
+               <p className='text-thin text-base'>LOB (Line of business)</p>
+               <p className='text-thin text-base'>Services</p>
+            </div>
+         </div>
+         <div class="  w-[2px] h-full bg-[#CBCBCB]"></div>
+         <div className=' w-1/5 h-full'>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Locations</h1>
+               <p className='text-thin text-base'>Country</p>
+               <p className='text-thin text-base'>State</p>
+               <p className='text-thin text-base'>City</p>
+               <p className='text-thin text-base'>Locality</p>
+            </div>
+         </div>
+         <div class="  w-[2px] h-full bg-[#CBCBCB]"></div>
+         <div className=' w-1/5 h-full'>
+            <div className='ml-5 mt-4 flex-col space-y-2'>
+               <h1 className='font-semibold text-xl	'>Data Management</h1>
+               <p className='text-thin text-base'>Delete by ID</p>
+            </div>
+         </div>
+      </div>
+      }
+      
     </>
 
   )
