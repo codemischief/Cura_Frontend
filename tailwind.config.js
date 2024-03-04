5aa1c38a5696239ce675580fc4c3dd2c70ae666d
shwethaserpro <shwetha@serpro.in>
2024-03-04 19:48:40 +0530

    made the navbar responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'accent-blue' : '#004DD7',
+      }
+    },
   },
   plugins: [],
 }
