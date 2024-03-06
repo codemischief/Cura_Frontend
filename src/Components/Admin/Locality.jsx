ddfdd9b74076cdc68dec6a972e7c75f5d85c6664
Abhishek Ghanekar <a8hiyt@gmail.com>
2024-03-06 13:49:26 +0530

    updated country city and locality page

--- src/Components/Admin/Locality.jsx
+++ src/Components/Admin/Locality.jsx
@@ -1,100 +1,192 @@
-import React from 'react'
+import React from 'react';
 import { Outlet, Link } from "react-router-dom";
-import Back from "../assets/back.png";
+import backLink from "../assets/back.png";
 import searchIcon from "../assets/searchIcon.png";
-import Next from "../assets/next.png";
-import Refresh from "../assets/refresh.png";
-import Download from "../assets/download.png";
-
+import nextIcon from "../assets/next.png";
+import refreshIcon from "../assets/refresh.png";
+import downloadIcon from "../assets/download.png";
+import { useState } from 'react';
+import Navbar from "../Navabar/Navbar";
+import Cross from "../assets/cross.png";
+import { Modal } from "@mui/material";
 const Locality = () => {
+  // we have the module here
+  //Validation of the form
+  const initialValues = {
+    localityName:"",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+   
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      setIsSubmit(true);
+    };
+    
+  const [isLocalityDialogue,setIsLocalityDialogue] = React.useState(false);
+  const handleOpen = () => {
+     setIsLocalityDialogue(true);
+  };
+  const handleClose = () => {
+    setIsLocalityDialogue(false);
+  }
   return (
     <div>
-      <div>
-        <Outlet />
-      </div>
-      <div className="mx-[10px] mt-[10px] px-[20px] pt-[10xp] bg-white w-[1260px] h-[500px] rounded-sm">
-        <div className="flex items-center gap-[600px] h-[45px]">
-          <div className="flex items-center gap-[10px]">
-            <div className=" w-[30px] h-[30px] bg-[#EBEBEB] rounded-full flex justify-center items-center">
-              <Link to="/dashboard">
-                <img className="w-[20px] h-[20px]" src={Back} alt="back" />
-              </Link>
-            </div>
-            <div>
-              <div className="text-md">Locality</div>
-              <div className="text-[12px]">Admin - Locality</div>
-            </div>
-          </div>
-          <div className="flex justify-center items-center gap-[20px]">
-            <div className="flex justify-center items-center">
-              <input
-                className="h-[36px] bg-[#F5F5F5] text-[#787878]"
-                type="text"
-                placeholder="  Search"
-              />
-              <div className="h-[36px] w-[40px] bg-[#004DD7] flex items-center justify-center rounded-r-lg">
-                <img className="h-[26px] " src={searchIcon} alt="search-icon" />
-              </div>
-            </div>
-            <button className="bg-[#004DD7] text-white h-[30px] w-[200px] rounded-lg">
-              Add New user   +
-            </button>
-          </div>
-        </div>
-        <hr className="m-[10px]" />
-        <div className="flex justify-between items-center mx-[40px]">
-          <input
-            className="bg-[#F5F5F5] h-[23px] w-[115px] rounded-md text-[11px]"
-            type="text"
-            placeholder="  Country"
-          />
-          <input
-            className="bg-[#F5F5F5] h-[23px] w-[115px] rounded-md text-[11px]"
-            type="text"
-            placeholder="  State"
-          />
-          <input
-            className="bg-[#F5F5F5] h-[23px] w-[115px] rounded-md text-[11px]"
-            type="text"
-            placeholder="  City"
-          />
-          <input
-            className="bg-[#F5F5F5] h-[23px] w-[115px] rounded-md text-[11px]"
-            type="text"
-            placeholder="  Locality"
-          />
-          <input
-            className="bg-[#F5F5F5] h-[23px] w-[115px] rounded-md text-[11px]"
-            type="text"
-            placeholder=" Search Id"
-          />
-        </div>
-        <footer >
-          <hr className="bg-[#CBCBCB] h-[2px] my-[8px]" />
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-[40px]">
-              <div className="flex items-center gap-[30px]">
-                <img className="w-[15px] h-[15px]" src={Back} alt="back" />
-                <div className="flex items-center gap-[10px]">
-                  <span>1</span>
-                  <span>2</span>
-                  <span>3</span>
-                  <span>4</span>
+      <Navbar/>
+      <div className='flex-col w-screen h-full  bg-white'>
+        <div className='flex-col'>
+            {/* this div will have all the content */}
+            <div className='w-full  flex-col px-6'>
+                {/* the top section of the div */}
+                <div className='h-1/2 w-full  flex justify-between items-center p-2  border-gray-300 border-b-2'>
+                   <div className='flex items-center space-x-3'>
+                      <div className='rounded-2xl  bg-[#EBEBEB] h-8 w-8 '>
+                          <img src={backLink} />
+                      </div>
+                        
+                      <div className='flex-col'>
+                          <h1>Locality</h1>
+                          <p>Admin &gt; Locality</p>
+                      </div>
+                   </div>
+                   <div className='flex space-x-2 items-center'>
+
+                        <div className='flex'>
+                            {/* search button */}
+                                <input
+                                    className="h-[36px] bg-[#EBEBEB] text-[#787878]"
+                                    type="text"
+                                    placeholder="  Search"
+                                />
+                                <div className="h-[36px] w-[40px] bg-[#004DD7] flex items-center justify-center rounded-r-lg">
+                                    <img className="h-[26px] " src={searchIcon} alt="search-icon" />
+                                </div>
+                        </div>
+
+                        <div>
+                            {/* button */}
+                            <button className="bg-[#004DD7] text-white h-[30px] w-[200px] rounded-lg" onClick={handleOpen}>
+                                Add New Locality +
+                            </button>
+                        </div>
+
+                   </div>
+                   
                 </div>
-                <img className="w-[15px] h-[15px]" src={Next} alt="next" />
-              </div>
-              <div className="flex items-center gap-[5px]">
-                <span className="text-[11px] text-[#787878]">Item per page</span>
-                <div className="w-[40px] h-[24px] border-[2px] rounded-md border-[#CBCBCB] text-[11px] text-[#787878] flex justify-center items-center">16</div>
-              </div>
+                <div className='h-12 w-full bg-white'>
+                  
+                </div>
             </div>
-            <div className="flex items-center gap-[10px]">
-              <button className="w-[80px] h-[28px] border-[2px] border-[#505050] rounded-md bg-[#F5F5F5] flex justify-center items-center text-[11px] gap-[4px]">Refresh <img className="w-[11px] h-[11px]" src={Refresh} alt="refresh" /></button>
-              <button className="w-[100px] h-[28px] border-[2px] border-[#505050] rounded-md bg-[#F5F5F5] flex justify-center items-center text-[11px] gap-[4px]">Download <img className="w-[11px] h-[11px]" src={Download} alt="download" /></button>
+
+            <div className='w-full h-[509.5px] bg-white px-6'>
+                <div className='w-full h-12 bg-[#F0F6FF] flex justify-between'>
+                   <div className='w-3/4 flex'>
+                      <div className='w-1/6 p-4'>
+                         <p>Sr. No</p>
+                      </div>
+                      <div className='w-5/6  p-4'>
+                        <p>Locality</p>
+                      </div>
+                   </div>
+                   <div className='w-1/6  flex'>
+                      <div className='w-1/2  p-4'>
+                          <p>ID</p>
+                      </div>
+                      <div className='w-1/2 0 p-4'>
+                          <p>Edit</p>
+                      </div>
+                   </div>
+                </div>
             </div>
-          </div>
-        </footer>
-      </div>
+            <div className='w-full h-12 flex justify-between justify-self-end px-6 '>
+                {/* footer component */}
+                <div className='ml-2'>
+                    <div className='flex items-center w-auto h-full'>
+                       {/* items */}
+                       <div className='h-12 flex justify-center items-center'>
+                          <img src={backLink} className='h-2/4'/>
+                       </div>
+                       <div className='flex space-x-1 mx-5'>
+                          {/* pages */}
+                          <div className='w-6 bg-[#DAE7FF] p-1 pl-2 rounded-md'>
+                              <p>1</p>  
+                          </div>
+                          <div className='w-6  p-1 pl-2'>
+                              <p>2</p> 
+                          </div>
+                          <div className='w-6 p-1 pl-2'>
+                              <p>3</p> 
+                          </div>
+                          <div className='w-6  p-1 pl-2'>
+                              <p>4</p> 
+                          </div>
+                       </div>
+                       <div className='h-12 flex justify-center items-center'>
+                        {/* right button */}
+                           <img src={nextIcon} className='h-2/4'/>
+                       </div>
+                    </div>
+                    <div>
+                        {/* items per page */}
+                    </div>
+                </div>
+                <div className='flex mr-10 justify-center items-center space-x-2 '>
+                    <div className='border-solid border-black border-[0.5px] rounded-md w-28 h-10 flex items-center justify-center space-x-1' >
+                       {/* refresh */}
+                       <p>Refresh</p>
+                       <img src={refreshIcon} className='h-1/2'/>
+                    </div>
+                    <div className='border-solid border-black border-[1px] w-28 rounded-md h-10 flex items-center justify-center space-x-1'>
+                        {/* download */}
+                        <p>Download</p>
+                        <img src={downloadIcon} className='h-1/2'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+    </div>
+
+    {/* modal goes here */}
+    <Modal open={isLocalityDialogue} 
+          fullWidth={true}
+          maxWidth = {'md'} >
+            <div className='flex justify-center mt-[200px]'>
+                <div className="w-6/7  h-[200px] bg-white rounded-lg">
+                    <div className="h-[40px] bg-[#EDF3FF]  justify-center flex items-center">
+                        <div className="mr-[410px] ml-[410px]">
+                            <div className="text-[16px]">Add New Locality</div>
+                        </div>
+                        <div className="flex justify-center items-center rounded-full w-[30px] h-[30px] bg-white">
+                            <button onClick={handleClose}><img  className="w-[20px] h-[20px]" src={Cross} alt="cross" /></button>
+                        </div>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="h-auto w-full mt-[5px] ">
+                            <div className="flex gap-[48px] justify-center items-center">
+                                <div className=" space-y-[12px] py-[20px] px-[10px]">
+                                    <div className="">
+                                        <div className="text-[14px]">Locality Name<label className="text-red-500">*</label></div>
+                                        <input className="w-[230px] h-[20px] border-[1px] border-[#C6C6C6] rounded-sm" type="text" name="empName"  value={formValues.localityName} onChange={handleChange}  />
+                                    </div>
+                                    
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="mt-[10px] flex justify-center items-center gap-[10px]">
+                            
+                            <button className='w-[100px] h-[35px] bg-[#004DD7] text-white rounded-md' type="submit">Save</button>
+                            <button className='w-[100px] h-[35px] border-[1px] border-[#282828] rounded-md' onClick={handleClose}>Cancel</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    </Modal>
     </div>
   )
 }
