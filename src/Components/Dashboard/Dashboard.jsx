3c0fa6645972af9375d2f1f8bbd3bed926002a08
Abhishek Ghanekar <a8hiyt@gmail.com>
2024-03-07 14:17:30 +0530

    dashboard changes

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import searchIcon from "../assets/searchIcon.png";
 import {Outlet} from "react-router-dom";
 import { useEffect , useState } from "react";
-
+import Navbar from "../Navabar/Navbar";
 const Dashboard = () => {
     const [myOrder, setmyorder] = useState([]);
     const [cashBalance, setcashbalance] = useState([]);
@@ -15,6 +15,7 @@ const Dashboard = () => {
                 .then((res) => res.json())
                 .then((data) =>{
                   setmyorder(data)
+                  console.log(data);
                 })
         fetch("/myCashBalance")
                 .then((res) => res.json())
@@ -41,98 +42,91 @@ const Dashboard = () => {
 
     
     return (
-        <div>
-            <div >
-                <Outlet />
-            </div>
-            <div className="ml-[35px] mt-[27px]">
-                
-            {/* //************ mock post request ************** */ }
-            {/* <input value={content} onChange={(c) => {setContent(c.target.value)}}/>
-                <button onClick={addCountryOnClick}>ADD</button> */}
-                <div className="flex space-x-[820px]">
-                    <div className="text-[28px]">Dashboard</div>
-                    <div className="flex w-[304px] h-[36px] items-center">
+        <div className="w-scren h-screen">
+            <Navbar/>
+            <div className="w-screen h-fit p-5">
+               {/* this is the background container */}
+               <div className="w-full h-full ">
+                  <div className="flex justify-between">
+                    <div >
+                       <h1 className="text-3xl font-sans">Dashboard</h1>
+                    </div>
+                    <div className="flex w-[220px] h-[36px] items-center">
                         <input className="h-[36px]" type="text" placeholder="  Search" />
                         <div className="h-[36px] w-[40px] bg-[#004DD7] flex items-center justify-center rounded-r-lg">
                             <img className="h-[26px] " src={searchIcon} alt="search-icon" />
                         </div>
                     </div>
-                </div>
-                <div className="mt-[15px] px-[27px] py-[15px] h-[240px] w-[1200px] bg-white rounded-md">
-                    <div className=" w-full h-[36px] bg-[#F0F6FF] rounded-lg flex items-center pl-[30px] text-[18px]">
-                        My Order
-                    </div>
-                    <table className="ml-[15px] mt-[7px] w-[1100px] h-[170px] ">
-                        <thead className="text-left">
-                            <tr>
-                                <th>Sr.</th>
-                                <th>Order Status</th>
-                                <th>Count</th>
-                            </tr>
-                        </thead> 
-                        {myOrder.map((item) => (
-                               <tbody>
-                                <tr>
-                                    <td>{item.sl}</td>
-                                    <td>{item.order_status}</td>
-                                    <td>{item.count}</td>
-                                </tr>
-                                </tbody> 
-                             ))}
-                        {/* <tbody>
-                            <tr>
-                                <td>01</td>
-                                <td>billed</td>
-                                <td>37</td>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <td>02</td>
-                                <td>Closed</td>
-                                <td>2898</td>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <td>03</td>
-                                <td>Cancelled</td>
-                                <td>28</td>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <td>04</td>
-                                <td>In progress</td>
-                                <td>182</td>
-                            </tr>
-                        </tbody> */}
-                    </table>
-                </div>
-                <div className="mt-[15px] px-[27px] py-[15px] h-[130px] w-[1200px] bg-white rounded-md">
-                    <div className=" w-full h-[36px] bg-[#F0F6FF] rounded-lg flex items-center pl-[30px] text-[18px]">
-                        My Cash Balance
-                    </div>
-                    <table className="ml-[15px] mt-[7px] w-[1500px] h-[60px] ">
-                        <thead className="text-left">
-                            <tr>
-                                <th>Sr.</th>
-                                <th>Username</th>
-                                <th>Balance</th>
-                            </tr>
-                        </thead>
-                        {cashBalance.map((item) => (
-                               <tbody>
-                                <tr>
-                                    <td>{item.Sr}</td>
-                                    <td>{item.Username}</td>
-                                    <td>{item.Balance}</td>
-                                </tr>
-                                </tbody> 
-                             ))}
-                    </table>
-                </div>
+                  </div>
+
+                  <div className="mt-2 bg-white w-full h-96 p-3">
+                     <div className="w-full h-full">
+                        {/* this is the internal div */}
+                        <div className="w-full h-[55px] bg-[#DAE7FF] pl-5 pt-3 rounded-md">
+                            {/* this will havw the header */}
+                            <h1 className="font-sans text-2xl">My Orders</h1>
+                        </div>
+                        <div className="w-full h-full "> 
+                            {/* this will have the items */}
+                             <div className=" w-full h-[45px] flex border-gray-400 border-b-[1px]">
+                                {/* this will have the index */}
+                                 <div className="w-[30%] h-full  p-3" >
+                                    <h1>SR No</h1>
+                                 </div>
+                                 <div className="w-[50%] h-full  p-3">
+                                    <h1>Order Status</h1>
+                                 </div>
+                                 <div className="w-[20%] h-full  p-3">
+                                   <h1>Count </h1>
+                                 </div>
+                             </div>
+
+                             {/* map the items here */}
+                             
+                             {myOrder.map((item) => {
+                                  return <div className=" w-full h-[45px] flex border-gray-400 border-b-[1px]">
+                                        
+                                    {/* this will have the index */}
+                                    <div className="w-[30%] h-full  p-3" >
+                                        <h1>{item.sl}</h1>
+                                    </div>
+                                    <div className="w-[50%] h-full  p-3">
+                                        <h1>{item.order_status}</h1>
+                                    </div>
+                                    <div className="w-[20%] h-full  p-3">
+                                    <h1>{item.count} </h1>
+                                    </div>
+                                    </div>
+                             })}
+                        </div>
+                     </div>
+                        <div className="mt-5 w-full h-48 ">
+                            <div className="w-full h-[55px] bg-[#DAE7FF] pl-5 pt-3 rounded-lg">
+                                {/* this will havw the header */}
+                                <h1 className="font-sans text-2xl">Cash Balance</h1>
+                            </div>
+                            {/* map the data here */}
+                            {cashBalance.map((item) => {
+                                  return <div className=" w-full h-[45px] flex border-gray-400 border-b-[1px]">
+                                        
+                                    {/* this will have the index */}
+                                    <div className="w-[30%] h-full  p-3" >
+                                        <h1>{item.Sr}</h1>
+                                    </div>
+                                    <div className="w-[50%] h-full  p-3">
+                                        <h1>{item.Username}</h1>
+                                    </div>
+                                    <div className="w-[20%] h-full  p-3">
+                                    <h1>{item.Balance} </h1>
+                                    </div>
+                                    </div>
+                             })}
+                      {/* this will be the cash balances section */}
+                        </div>
+                  </div>
+                  
+               </div>
+               
             </div>
         </div>
     );
