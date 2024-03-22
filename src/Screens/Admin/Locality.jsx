f26720afb42a684b3dda30491faf3c7faa4c2108
shwethaserpro <shwetha@serpro.in>
2024-03-22 13:54:23 +0530

    Replication pagination
    # Conflicts:
    #	src/services/API.js

--- src/Screens/Admin/Locality.jsx
+++ src/Screens/Admin/Locality.jsx
@@ -10,12 +10,49 @@ import Navbar from "../../Components/Navabar/Navbar";
 import Cross from "../../assets/cross.png";
 import Edit from "../../assets/edit.png";
 import Trash from "../../assets/trash.png";
-import { Modal } from "@mui/material";
 import * as XLSX from 'xlsx';
 import FileSaver from 'file-saver';
+import { Modal, CircularProgress, Pagination } from "@mui/material";
+import Filter from "../../assets/filter.png"
 const Locality = () => {
     // we have the module here
     const [existingLocalities, setExistingLocalities] = useState([]);
+    const [currentPages,setCurrentPages] = useState(15);
+    const [currentPage,setCurrentPage] = useState(1);
+    const [totalItems,setTotalItems] = useState(0);
+    const [downloadModal,setDownloadModal] = useState(false);
+    const [userId, setUserId]=useState(0);
+    const fetchUserId = async() =>{
+        const response = await authService.getUserId();
+        setUserId(response)
+    }
+    const initialSelectedFields = {
+        name : {
+            selected : false,
+            queryType : "",
+        },
+        id : {
+            selected : false,
+            queryType : ""
+        }}
+    const fetchLocalityData = async () => {
+            setPageLoading(true);
+            const data = { 
+              "user_id" : user_id || 1234,
+              "rows" : ["id","name"],
+              "filters" : [],
+              "sort_by" : [],
+              "order" : "asc",
+              "pg_no" : 1,
+              "pg_size" : 15
+           };
+            const response = await APIService.getLocality(data)
+            const result = (await response.json()).data;
+            const t = result.total_count;
+            setTotalItems(t);
+            setExistingLocalities(result)
+            setPageLoading(false);
+          }
     const fetchData = () => {
         fetch("/getlocality")
             .then((res) => res.json())
@@ -25,7 +62,8 @@ const Locality = () => {
             })
     }
     useEffect(() => {
-        fetchData();
+        fetchUserId()
+        fetchLocalityData();
     }, []);
     //Validation of the form
     const initialValues = {
@@ -59,6 +97,58 @@ const Locality = () => {
       }
     const handleRefresh = () => {
         fetchData();
+    }
+    const [flag,setFlag] = useState(true);
+    const handleSort = async (field) => {
+          setPageLoading(true);
+          const data = { 
+              "user_id" : userId|| 1234,
+              "rows" : ["id","name"],
+              "filters" : [],
+              "sort_by" : [field],
+              "order" : flag ? "asc" : "desc",
+              "pg_no" : 1,
+              "pg_size" : Number(currentPages)
+           };
+           const response = await APIService.getLocality(data)
+           const result = (await response.json()).data;
+           const t = result.total_count;
+           setTotalItems(t);
+           setExistingLocalities(result)
+           setPageLoading(false);
+    }
+    const handleSearch = async () => {}
+    const [localityFilter,setLocalityFilter] = useState(false);
+    const [localityFilterInput,setLocalityFilterInput] = useState("");
+    const toggleLocalityFilter = () => {
+         setLocalityFilter((prev) => !prev)
+    }
+    const fetchFiltered = async (filterType,filterField) => {
+      const filterArray = [];
+      
+      setPageLoading(true);
+      const data = { 
+          "user_id" : userId || 1234,
+          "rows" : ["id","name"],
+          "filters" : [["name",String(filterType),localityFilterInput]],
+          "sort_by" : [],
+          "order" : "asc",
+          "pg_no" : 1,
+          "pg_size" : Number(currentPages)
+       };
+       const response = await APIService.getLocality(data)
+       const result = (await response.json()).data;
+       const t = result.total_count;
+       setTotalItems(t);
+       setExistingLocalities(result)
+       setPageLoading(false);
+      setFlag((prev) => {
+          return !prev;
+      })
+      setPageLoading(false);
+    }
+    const handlePageChange = (event,value) => {
+       setCurrentPage(value)
     }
     return (
         <div>
@@ -103,8 +193,52 @@ const Locality = () => {
                             </div>
 
                         </div>
-                        <div className='h-12 w-full bg-white'>
-
+                        <div className='h-12 w-full bg-white flex justify-between'>
+                             <div className='w-3/4 flex'>
+                                <div className='w-[10%] p-4'>
+                                    
+                                </div>
+                                <div className='w-[20%] p-4'>
+                                   <input className="w-14 bg-[#EBEBEB]" value={localityFilterInput} onChange={(e) => setLocalityFilterInput(e.target.value)}/>
+                                   <button className='p-1' onClick={toggleLocalityFilter}><img src={Filter} className='h-[17px] w-[17px]'/></button>
+                                   {localityFilter && <div className='h-[270px] w-[150px] mt-3 bg-white shadow-xl font-thin font-sans absolute p-2 flex-col rounded-md space-y-1 text-sm'>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
+                                              <h1 >No Filter</h1>
+                                            </div>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
+                                              <button onClick={() => fetchFiltered('contains')}><h1 >Contains</h1></button>
+                                            </div>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
+                                            <button onClick={() => fetchFiltered('contains')}><h1 >DoesNotContain</h1></button>
+                                            </div>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
+                                            <button onClick={() => fetchFiltered('startsWith')}><h1 >StartsWith</h1></button>
+                                            </div>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer '>
+                                            <button onClick={() => fetchFiltered('endsWith')}><h1 >EndsWith</h1></button>
+                                            </div>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
+                                            <button onClick={() => fetchFiltered('exactMatch')}><h1 >EqualTo</h1></button>
+                                            </div>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
+                                               <button onClick={() => fetchFiltered('isNull')}><h1 >isNull</h1></button>
+                                            </div>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
+                                               <button onClick={() => fetchFiltered('isNotNull')}><h1 >NotIsNull</h1></button>
+                                            </div>
+                                        </div>} 
+                                </div>
+                                
+                            </div>
+                            <div className='w-1/6  flex'>
+                                <div className='w-[50%] p-2 mt-2'>
+                                   <input className="w-14 bg-[#EBEBEB]"/>
+                                   <button className='p-1'><img src={Filter} className='h-[17px] w-[17px]'/></button>
+                                </div>
+                                <div className='w-1/2 0 p-4'>
+                                     
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -115,21 +249,21 @@ const Locality = () => {
                                     <p>Sr. </p>
                                 </div>
                                 <div className='w-[15%]  p-4'>
-                                    <p>Country</p>
+                                    <p>Country <button onClick={() => handleSort("name")}><span className="font-extrabold">↑↓</span></button></p>
                                 </div>
                                 <div className='w-[20%]  p-4'>
-                                    <p>State</p>
+                                    <p>State <button onClick={() => handleSort("name")}><span className="font-extrabold">↑↓</span></button></p>
                                 </div>
                                 <div className='w-[20%]  p-4'>
-                                    <p>City</p>
+                                    <p>City <button onClick={() => handleSort("name")}><span className="font-extrabold">↑↓</span></button></p>
                                 </div>
                                 <div className='w-[25%]  p-4'>
-                                    <p>Locality</p>
+                                    <p>Locality <button onClick={() => handleSort("name")}><span className="font-extrabold">↑↓</span></button></p>
                                 </div>
                             </div>
                             <div className='w-[15%] flex'>
                                 <div className='w-1/2  p-4'>
-                                    <p>ID</p>
+                                    <p>ID<button onClick={() => handleSort("id")}><span className="font-extrabold">↑↓</span></button> </p>
                                 </div>
                                 <div className='w-1/2 0 p-4'>
                                     <p>Edit</p>
@@ -171,50 +305,65 @@ const Locality = () => {
 
                         </div>
                     </div>
-                    <div className='w-full h-12 flex justify-between justify-self-end px-6 '>
+                    <div className='w-full h-12 flex justify-between justify-self-end px-6 mt-5 fixed bottom-0 '>
                         {/* footer component */}
                         <div className='ml-2'>
                             <div className='flex items-center w-auto h-full'>
                                 {/* items */}
-                                <div className='h-12 flex justify-center items-center'>
-                                    <img src={backLink} className='h-2/4' />
-                                </div>
-                                <div className='flex space-x-1 mx-5'>
-                                    {/* pages */}
-                                    <div className='w-6 bg-[#DAE7FF] p-1 pl-2 rounded-md'>
-                                        <p>1</p>
-                                    </div>
-                                    <div className='w-6  p-1 pl-2'>
-                                        <p>2</p>
-                                    </div>
-                                    <div className='w-6 p-1 pl-2'>
-                                        <p>3</p>
-                                    </div>
-                                    <div className='w-6  p-1 pl-2'>
-                                        <p>4</p>
-                                    </div>
-                                </div>
-                                <div className='h-12 flex justify-center items-center'>
-                                    {/* right button */}
-                                    <img src={nextIcon} className='h-2/4' />
-                                </div>
-                            </div>
-                            <div>
-                                {/* items per page */}
+                                <Pagination count={Math.ceil(totalItems/currentPages)} onChange={handlePageChange} page={currentPage}/>
+                                
                             </div>
                         </div>
                         <div className='flex mr-10 justify-center items-center space-x-2 '>
-                            <div className='border-solid border-black border-[0.5px] rounded-md w-28 h-10 flex items-center justify-center space-x-1' >
+                            <div className="flex mr-8 space-x-2 text-sm items-center">
+                               <p className="text-gray-700">Items Per page</p>
+                               <select className="text-gray-700 border-black border-[1px] rounded-md p-1"
+                                         name="currentPages"
+                                         value={currentPages}
+                                        //  defaultValue="Select State"
+                                         onChange={e => {
+                                            setCurrentPages(e.target.value);
+                                            console.log(e.target.value);
+                                            fetchSomeData(e.target.value)
+                                         }}
+                               
+                               >
+                                <option>
+                                    15
+                                </option>
+                                <option>
+                                    20
+                                </option>
+                                <option>
+                                    25
+                                </option>
+                               </select>
+                            </div>
+                            <div className="flex text-sm">
+                                <p className="mr-11 text-gray-700">{totalItems} Items in {Math.ceil(totalItems/currentPages)} Pages</p>
+                            </div>
+                            {downloadModal && <div className='h-[130px] w-[200px] bg-red-800 absolute bottom-12 right-24 flex-col items-center  justify-center space-y-6 p-5'>
+                               
+                               <div className='flex'>
+                                 <p>Download as pdf</p>
+                                 {/* <img src=''/> */}
+                               </div>
+                               <div>
+                                  <p>Download as Excel</p>
+                               </div>
+                            </div>}
+                            
+                            <div className='border-solid border-black border-[0.5px] rounded-md w-28 h-10 flex items-center justify-center space-x-1 p-2' >
                                 {/* refresh */}
                                 <button onClick={handleRefresh}><p>Refresh</p></button>
-                                <img src={refreshIcon} className='h-1/2' />
+                                <img src={refreshIcon} className="h-2/3" />
                             </div>
-                            <div className='border-solid border-black border-[1px] w-28 rounded-md h-10 flex items-center justify-center space-x-1'>
+                            <div className='border-solid border-black border-[1px] w-28 rounded-md h-10 flex items-center justify-center space-x-1 p-2'>
                                 {/* download */}
                                 <button onClick={handleDownload}><p>Download</p></button>
-                                <img src={downloadIcon} className='h-1/2' />
+                                <img src={downloadIcon} className="h-2/3" />
                             </div>
-                        </div>
+                        </div> 
                     </div>
                 </div>
 
--- src/services/API.js
+++ src/services/API.js
@@ -106,7 +106,13 @@ return response;
   return response;
  }
 
+ const getLocality = async (data) => {
+  const response = await fetch(`${env_URL_SERVER}getLocality`, METHOD_POST(data));
+  return response;
+ }
+
 
 
  export  const APIService = { getCountries, getNewBuilderInfo, editCountry, addCountries, getState, getCities,addNewBuilder,editBuilderInfo,deleteBuilderInfo,deleteCountries,
   getStatesAdmin, getCitiesAdmin,getLob,addLob,editLob,getBankStatement, editBankStatement, editBankStatement,deleteBankStatement,getVendorAdmin,deleteLob};
+  getStatesAdmin, getCitiesAdmin,getLob,addLob,getBankStatement, editBankStatement, editBankStatement,deleteBankStatement,getVendorAdmin,deleteLob, getLocality};
