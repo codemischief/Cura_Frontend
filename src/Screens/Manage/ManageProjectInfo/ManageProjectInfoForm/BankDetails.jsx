4f726da847fa65bc33d9fc664bdbc76f5c7de66b
Abhishek Ghanekar <119523976+abhishek-ghanekar@users.noreply.github.com>
2024-03-23 13:41:46 +0530

    Update BankDetails.jsx

--- src/Screens/Manage/ManageProjectInfo/ManageProjectInfoForm/BankDetails.jsx
+++ src/Screens/Manage/ManageProjectInfo/ManageProjectInfoForm/BankDetails.jsx
@@ -1,39 +1,55 @@
 import React from 'react'
-
+import {useState} from 'react;
 const BankDetails = () => {
+  const [addField, setAddFeild] = useState([]);
+  const handleAdd = () => {
+    const abc = [...addField, []];
+    setAddFeild(abc);
+  }
   return (
-    <div className='flex-col justify-center items-center mx-5 mt-10 '>
-      <div className="w-full h-full">
-        <div className="w-full h-[40px] bg-[#EBEBEB] flex border-[#CBCBCB] border-b-[1px]">
-          <div className="w-[3%] h-full p-3 text-[11px]" >
-            Sr. 
-          </div>
-          <div className="w-[15%] h-full p-3 text-[11px]" >
-            Name 
-          </div>
-          <div className="w-[13%] h-full p-3 text-[11px]" >
-            Branch 
-          </div>
-          <div className="w-[13%] h-full p-3 text-[11px]" >
-            City 
-          </div>
-          <div className="w-[17%] h-full p-3 text-[11px]" >
-            Acc.Holder Name 
-          </div>
-          <div className="w-[14%] h-full p-3 text-[11px]" >
-            Acc.No 
-          </div>
-          <div className="w-[15%] h-full p-3 text-[11px]" >
-            Account Type 
-          </div>
-          <div className="w-[17%] h-full p-3 text-[11px]" >
-            IFSC Code  
-          </div>
-          <div className="w-[20%] h-full p-3 text-[11px]" >
-            MICR Code 
+    <div>
+      <form >
+        <div className='flex-col justify-center items-center mx-5 mt-10'>
+          <div className="w-full h-full">
+            <div className="w-full h-[40px] bg-[#EBEBEB] flex border-[#CBCBCB] border-b-[1px] ">
+              <div className="w-[7%] h-full p-3 text-[11px]" >
+                Sr.
+              </div>
+              <div className="w-[31%] h-full p-3 text-[11px]" >
+                Photo Link
+              </div>
+              <div className="w-[31%] h-full p-3 text-[11px]" >
+                Description
+              </div>
+              <div className="w-[31%] h-full p-3 text-[11px]" >
+                Photo Taken Date
+              </div>
+            </div>
+            <div className="w-full h-[40px] flex border-[#CBCBCB] border-b-[1px]">
+              <div className="w-[7%] h-full p-3 text-[11px]" >
+                1
+              </div>
+              <div className="w-[31%] h-full p-1 text-[11px]" >
+                <input className='w-full h-full bg-[#F5F5F5]' type="text" placeholder=' Paste the hyperlink here' />
+              </div>
+              <div className="w-[31%] h-full p-1 text-[11px]" >
+                <input className='w-full h-full bg-[#F5F5F5]' type="text" />
+              </div>
+              <div className="w-[31%] h-full p-1 text-[11px]" >
+                <input className='w-full h-full bg-[#F5F5F5]' type="date" />
+              </div>
+            </div>
+            {addField.map((data, index) => {
+              return (
+                <AddFeildPhotos index={index} />
+              )
+            })}
+            <div className="w-full h-full bg-[#E6ECF5] cursor-pointer p-2 mt-1 flex justify-center items-center">
+              <button className='text-[15px]' onClick={() => handleAdd()}>ADD  +</button>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
