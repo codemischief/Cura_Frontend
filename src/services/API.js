904535e95a0c50f283d2e00270b06b217c4fcf1d
Abhishek Ghanekar <a8hiyt@gmail.com>
2024-03-27 09:22:23 +0530

    made delete employee

--- src/services/API.js
+++ src/services/API.js
@@ -168,7 +168,11 @@ return response;
   const response = await fetch(`${env_URL_SERVER}getRolesAdmin`, METHOD_POST(data));
   return response;
  }
+ const deleteEmployee = async (data) => {
+  const response = await fetch(`${env_URL_SERVER}deleteEmployee`, METHOD_POST(data));
+  return response;
+ }
  export  const APIService = { getCountries, getNewBuilderInfo, editCountry, addCountries, getState, getCities,addNewBuilder,editBuilderInfo,deleteBuilderInfo,deleteCountries,
   getStatesAdmin, getCitiesAdmin,getLob,addLob,editLob,getBankStatement, editBankStatement,deleteBankStatement,getVendorAdmin,deleteLob,getEmployees,addEmployee,
-  getLocality,addBankStatement,addLocality,deleteLocality,getModesAdmin,getEntityAdmin,getHowReceivedAdmin,getClientAdmin,addClientReceipt,getPayment,getProjectInfo,editLocality,getUsers,getRoles};
+  getLocality,addBankStatement,addLocality,deleteLocality,getModesAdmin,getEntityAdmin,getHowReceivedAdmin,getClientAdmin,addClientReceipt,getPayment,getProjectInfo,editLocality,getUsers,getRoles,deleteEmployee};
   
