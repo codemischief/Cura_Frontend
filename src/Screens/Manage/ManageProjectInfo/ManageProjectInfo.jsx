9be7a1cb8aa74fb9e61f7074654e275839780a62
Zamster44 <114329974+Zamster44@users.noreply.github.com>
2024-04-05 01:42:10 +0530

    Update ManageProjectInfo.jsx

--- src/Screens/Manage/ManageProjectInfo/ManageProjectInfo.jsx
+++ src/Screens/Manage/ManageProjectInfo/ManageProjectInfo.jsx
@@ -338,56 +338,55 @@ const ManageProjectInfo = () => {
         setPageLoading(false);
     }
     return (
-        <div>
+        <div className="h-screen">
             <Navbar />
-            <div className='flex-col w-full h-full  bg-white'>
-                <div className='flex-col'>
-                    {/* this div will have all the content */}
-                    <div className='w-full  flex-col px-6'>
-                        {/* the top section of the div */}
-                        <div className='h-1/2 w-full  flex justify-between items-center p-2  border-gray-300 border-b-2'>
-                            <div className='flex items-center space-x-3'>
-                                <div className='rounded-2xl  bg-[#EBEBEB] h-8 w-8 flex justify-center items-center'>
-                                    <img className='w-5 h-5 ' src={backLink} />
-                                </div>
 
-                                <div className='flex-col'>
-                                    <h1 className='text-[18px]'>Manage Project</h1>
-                                    <p className='text-[14px]'>Manage &gt; Manage Project</p>
-                                </div>
-                            </div>
-                            <div className='flex space-x-2 items-center'>
+            <div className='h-[calc(100vh_-_7rem)] w-full px-10'>
 
-                                <div className='flex relative'>
-                                    {/* search button */}
-                                    <input
-                                        className="h-[36px] bg-[#EBEBEB] text-[#787878]"
-                                        type="text"
-                                        placeholder="  Search"
-                                        value={searchInput}
-                                        onChange={(e) => {
-                                            setSearchInput(e.target.value);
-                                        }}
-                                    />
-                                    <button onClick={handleCloseSearch}><img src={Cross} className='absolute w-[20px] h-[20px] left-[160px] top-2' /></button>
-                                    <div className="h-[36px] w-[40px] bg-[#004DD7] flex items-center justify-center rounded-r-lg">
-                                        <button onClick={handleSearch}><img className="h-[26px] " src={searchIcon} alt="search-icon" /></button>
-                                    </div>
-                                </div>
-
-                                <div>
-                                    {/* button */}
-                                    <button className="bg-[#004DD7] text-white h-[36px] w-[250px] rounded-lg" onClick={handleOpen}>
-                                        <div className="flex items-center justify-center gap-4">
-                                            Add New Project
-                                            <img className='h-[18px] w-[18px]' src={Add} alt="add" />
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-
+                <div className='h-16 w-full  flex justify-between items-center p-2  border-gray-300 border-b-2'>
+                    <div className='flex items-center space-x-3'>
+                        <div className='rounded-2xl  bg-[#EBEBEB] h-8 w-8 flex justify-center items-center'>
+                            <img className='w-5 h-5 ' src={backLink} />
                         </div>
-                        <div className='h-12 w-full bg-white'>
+
+                        <div className='flex-col'>
+                            <h1 className='text-[18px]'>Manage Project</h1>
+                            <p className='text-[14px]'>Manage &gt; Manage Project</p>
+                        </div>
+                    </div>
+                    <div className='flex space-x-2 items-center'>
+
+                        <div className='flex relative'>
+                            {/* search button */}
+                            <input
+                                className="h-[36px] bg-[#EBEBEB] text-[#787878]"
+                                type="text"
+                                placeholder="  Search"
+                                value={searchInput}
+                                onChange={(e) => {
+                                    setSearchInput(e.target.value);
+                                }}
+                            />
+                            <button onClick={handleCloseSearch}><img src={Cross} className='absolute w-[20px] h-[20px] left-[160px] top-2' /></button>
+                            <div className="h-[36px] w-[40px] bg-[#004DD7] flex items-center justify-center rounded-r-lg">
+                                <button onClick={handleSearch}><img className="h-[26px] " src={searchIcon} alt="search-icon" /></button>
+                            </div>
+                        </div>
+
+                        <div>
+                            {/* button */}
+                            <button className="bg-[#004DD7] text-white h-[36px] w-[250px] rounded-lg" onClick={handleOpen}>
+                                <div className="flex items-center justify-center gap-4">
+                                    Add New Project
+                                    <img className='h-[18px] w-[18px]' src={Add} alt="add" />
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='h-12 w-full bg-white'>
                             <div className='w-full h-12 bg-white flex justify-between'>
                                 <div className="w-[85%] flex">
                                     <div className='w-[4%] flex'>
@@ -457,9 +456,9 @@ const ManageProjectInfo = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className='w-full h-[400px] bg-white px-6  text-[12px]'>
+                        <div className='h-[calc(100vh_-_14rem)] w-full text-[14px]'>
+
                         <div className='w-full h-12 bg-[#F0F6FF] flex justify-between'>
                             <div className="w-[85%] flex">
                                 <div className='w-[4%] flex'>
@@ -522,7 +521,8 @@ const ManageProjectInfo = () => {
                             </div>
 
                         </div>
-                        <div className='w-full h-80 overflow-auto'>
+
+                        <div className='w-full h-[calc(100vh_-_17rem)] overflow-auto'>
                             {/* we map our items here */}
                             {pageLoading && <div className='ml-5 mt-5'><LinearProgress /></div>}
                             {!pageLoading && existingProjectInfo.map((item, index) => {
@@ -589,81 +589,80 @@ const ManageProjectInfo = () => {
                                 </div>
                             })}
                         </div>
-                    </div>
 
-                    <div className='w-full h-12 flex justify-between justify-self-end px-6 mt-5 fixed bottom-0 bg-white '>
-                        {/* footer component */}
-                        <div className='ml-2'>
-                            <div className='flex items-center w-auto h-full'>
-                                {/* items */}
-                                <Pagination count={Math.ceil(totalItems / currentPages)} onChange={handlePageChange} page={currentPage} />
-
-                            </div>
                         </div>
-                        <div className='flex mr-10 justify-center items-center space-x-2 '>
-                            <div className="flex mr-8 space-x-2 text-sm items-center">
-                                <p className="text-gray-700">Items Per page</p>
-                                <select className="text-gray-700 border-black border-[1px] rounded-md p-1"
-                                    name="currentPages"
-                                    value={currentPages}
-                                    //  defaultValue="Select State"
-                                    onChange={e => {
-                                        setCurrentPages(e.target.value);
-                                        console.log(e.target.value);
-
-                                        fetchQuantityData(e.target.value)
-                                    }}
-
-                                >
-                                    <option>
-                                        15
-                                    </option>
-                                    <option>
-                                        25
-                                    </option>
-                                    <option>
-                                        50
-                                    </option>
-                                </select>
-                            </div>
-                            <div className="flex text-sm">
-                                <p className="mr-11 text-gray-700">{totalItems} Items in {Math.ceil(totalItems / currentPages)} Pages</p>
-                            </div>
-                            {downloadModal && <div className='h-[120px] w-[220px] bg-white shadow-xl rounded-md absolute bottom-12 right-24 flex-col items-center justify-center  p-5'>
-                                <button onClick={() => setDownloadModal(false)}><img src={Cross} className='absolute top-1 left-1 w-4 h-4' /></button>
-
-                                <button>
-                                    <div className='flex space-x-2 justify-center items-center ml-3 mt-3'>
-
-                                        <p>Download as pdf</p>
-                                        <img src={Pdf} />
-                                    </div>
-                                </button>
-                                <button onClick={handleExcelDownload}>
-                                    <div className='flex space-x-2 justify-center items-center mt-5 ml-3'>
-                                        <p>Download as Excel</p>
-                                        <img src={Excel} />
-                                    </div>
-                                </button>
-                            </div>}
-
-                            <div className='border-solid border-black border-[0.5px] rounded-md w-28 h-10 flex items-center justify-center space-x-1 p-2' >
-                                {/* refresh */}
-                                <button onClick={handleRefresh}><p>Refresh</p></button>
-                                <img src={refreshIcon} className="h-2/3" />
-                            </div>
-                            <div className='border-solid border-black border-[1px] w-28 rounded-md h-10 flex items-center justify-center space-x-1 p-2'>
-                                {/* download */}
-                                <button onClick={openDownload}><p>Download</p></button>
-                                <img src={downloadIcon} className="h-2/3" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
             </div>
 
-            {/* modal goes here */}
+            <div className='w-full h-12 flex justify-between px-6 bg-white '>
+                {/* footer component */}
+                <div className='ml-2'>
+                    <div className='flex items-center w-auto h-full'>
+                        {/* items */}
+                        <Pagination count={Math.ceil(totalItems / currentPages)} onChange={handlePageChange} page={currentPage} />
+
+                    </div>
+                </div>
+                <div className='flex mr-10 justify-center items-center space-x-2 '>
+                    <div className="flex mr-8 space-x-2 text-sm items-center">
+                        <p className="text-gray-700">Items Per page</p>
+                        <select className="text-gray-700 border-black border-[1px] rounded-md p-1"
+                            name="currentPages"
+                            value={currentPages}
+                            //  defaultValue="Select State"
+                            onChange={e => {
+                                setCurrentPages(e.target.value);
+                                console.log(e.target.value);
+
+                                fetchQuantityData(e.target.value)
+                            }}
+
+                        >
+                            <option>
+                                15
+                            </option>
+                            <option>
+                                25
+                            </option>
+                            <option>
+                                50
+                            </option>
+                        </select>
+                    </div>
+                    <div className="flex text-sm">
+                        <p className="mr-11 text-gray-700">{totalItems} Items in {Math.ceil(totalItems / currentPages)} Pages</p>
+                    </div>
+                    {downloadModal && <div className='h-[120px] w-[220px] bg-white shadow-xl rounded-md absolute bottom-12 right-24 flex-col items-center justify-center  p-5'>
+                        <button onClick={() => setDownloadModal(false)}><img src={Cross} className='absolute top-1 left-1 w-4 h-4' /></button>
+
+                        <button>
+                            <div className='flex space-x-2 justify-center items-center ml-3 mt-3'>
+
+                                <p>Download as pdf</p>
+                                <img src={Pdf} />
+                            </div>
+                        </button>
+                        <button onClick={handleExcelDownload}>
+                            <div className='flex space-x-2 justify-center items-center mt-5 ml-3'>
+                                <p>Download as Excel</p>
+                                <img src={Excel} />
+                            </div>
+                        </button>
+                    </div>}
+
+                    <div className='border-solid border-black border-[0.5px] rounded-md w-28 h-10 flex items-center justify-center space-x-1 p-2' >
+                        {/* refresh */}
+                        <button onClick={handleRefresh}><p>Refresh</p></button>
+                        <img src={refreshIcon} className="h-2/3" />
+                    </div>
+                    <div className='border-solid border-black border-[1px] w-28 rounded-md h-10 flex items-center justify-center space-x-1 p-2'>
+                        {/* download */}
+                        <button onClick={openDownload}><p>Download</p></button>
+                        <img src={downloadIcon} className="h-2/3" />
+                    </div>
+                </div>
+            </div>
+
             <Modal open={isStateDialogue}
                 fullWidth={true}
                 maxWidth={'md'}
@@ -710,6 +709,7 @@ const ManageProjectInfo = () => {
                     </div>
                 </>
             </Modal>
+            
         </div>
     )
 }
