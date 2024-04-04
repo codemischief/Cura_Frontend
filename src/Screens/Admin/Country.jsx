738045eff8f43270d4eaa7bbd6c752a31329a79e
Abhishek Ghanekar <119523976+abhishek-ghanekar@users.noreply.github.com>
2024-04-04 11:58:56 +0530

    Update Country.jsx

--- src/Screens/Admin/Country.jsx
+++ src/Screens/Admin/Country.jsx
@@ -398,6 +398,10 @@ const Country = () => {
   return ( 
     <div className='h-screen w-full'>
         <Navbar/>
+        <SucessfullModal isOpen={showSucess} message="Country Added Successfully" />
+      <FailureModal isOpen={showFailure} message={failureMessage} />
+      <DeleteModal isOpen={showDelete} currentCountry={currentCountry} closeDialog={setShowDelete} fetchData={fetchCountryData} />
+      <EditCountryModal isOpen={showEdit} currentCountry={currentCountry} setIsOpen={setShowEdit}/>
          <div className='h-[calc(100vh_-_7rem)] w-full px-10'>
              
              
@@ -638,6 +642,47 @@ const Country = () => {
                             </div>
                         </div> 
                     </div>
+           <Modal open={isCountryDialogue}
+        fullWidth={true}
+        maxWidth={'md'} >
+        <div className='flex justify-center mt-[200px]'>
+          <div className="w-6/7  h-[250px] bg-white rounded-lg">
+            <div className="h-[40px] bg-[#EDF3FF]  justify-center flex items-center">
+              <div className="mr-[410px] ml-[410px]">
+                <div className="text-[16px]">Add New Country</div>
+              </div>
+              <div className="flex justify-center items-center rounded-full w-[30px] h-[30px] bg-white">
+                <button onClick={handleClose}><img className="w-[20px] h-[20px]" src={Cross} alt="cross" /></button>
+              </div>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="h-auto w-full mt-[5px] ">
+                <div className="flex gap-[48px] justify-center items-center">
+                  <div className=" space-y-[12px] py-[20px] px-[10px]">
+                    <div className="">
+                      <div className="text-[14px]">Country Name<label className="text-red-500">*</label></div>
+                      <input className="w-[230px] h-[20px] border-[1px] border-[#C6C6C6] rounded-sm"
+                        type="text"
+                        name="countryName"
+                        value={formValues.countryName}
+                        onChange={handleChange}
+                        autoComplete="off"
+                      />
+                      <div className="text-[12px] text-[#CD0000] ">{formErrors.countryName}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-[10px] flex justify-center items-center gap-[10px]">
+                <button className='w-[100px] h-[35px] bg-[#004DD7] text-white rounded-md' type="submit">Save</button>
+                <button className='w-[100px] h-[35px] border-[1px] border-[#282828] rounded-md' onClick={handleClose}>Cancel</button>
+                {isLoading && <CircularProgress />}
+              </div>
+            </form>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
