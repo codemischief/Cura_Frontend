4f1c6e223b2dbffca315c9f9bc7f8b5102eed7dd
Abhishek Ghanekar <a8hiyt@gmail.com>
2024-05-09 14:09:37 +0530

    updates

--- src/Components/Filters/NumericFilter.jsx
+++ src/Components/Filters/NumericFilter.jsx
@@ -2,40 +2,62 @@ import React from 'react'
 
 const NumericFilter = ({inputVariable,setInputVariable,handleFilter,columnName,menuRef}) => {
   return (
-    <div className='h-[360px] w-[150px] mt-3 bg-white shadow-xl font-thin font-sans absolute p-2 flex-col rounded-md space-y-1 text-sm z-40' ref={menuRef}>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'noFilter', columnName)}><h1 >No Filter</h1></button>
+    <div className='h-[400px] w-[150px] mt-3 bg-white shadow-xl font-thin font-sans absolute flex-col rounded-md space-y-0 text-sm z-40' ref={menuRef}>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'noFilter', columnName)} className='w-[150px] p-1'>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'> 
+                                                    <h1 >No Filter</h1> 
+                                            </div>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'equalTo', columnName)} className='w-[150px] p-1'>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                                    <h1 >EqualTo</h1>
+                                            </div>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'notEqualTo',columnName)} className='w-[150px] p-1'>
+                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                            <h1 >NotEqualTo</h1>
                                     </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'equalTo', columnName)}><h1 >EqualTo</h1></button>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'greaterThan', columnName)} className='w-[150px] p-1'>
+                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                            <h1 >GreaterThan</h1>
                                     </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'notEqualTo',columnName)}><h1 >NotEqualTo</h1></button>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'lessThan', columnName)} className='w-[150px] p-1'>
+                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start '>
+                                            <h1 >LessThan</h1>
                                     </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'greaterThan', columnName)}><h1 >GreaterThan</h1></button>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'greaterThanOrEqualTo', columnName)} className='w-[150px] p-1'>
+                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                            <h1 >GreaterThanOrEqualTo</h1>
                                     </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer '>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'lessThan', columnName)}><h1 >LessThan</h1></button>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'lessThanOrEqualTo', columnName)} className='w-[150px] p-1'>
+                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                            <h1 >LessThanOrEqualTo</h1>
                                     </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'greaterThanOrEqualTo', columnName)}><h1 >GreaterThanOrEqualTo</h1></button>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'between', columnName)} className='w-[150px] p-1'>
+                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                            <h1 >Between</h1>
                                     </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'lessThanOrEqualTo', columnName)}><h1 >LessThanOrEqualTo</h1></button>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'notBetween', columnName)} className='w-[150px] p-1'>
+                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                            <h1 >NotBetween</h1>
                                     </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'between', columnName)}><h1 >Between</h1></button>
-                                    </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'notBetween', columnName)}><h1 >NotBetween</h1></button>
-                                    </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'isNull', columnName)}><h1 >isNull</h1></button>
-                                    </div>
-                                    <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer'>
-                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'isNotNull', columnName)}><h1 >isNotNull</h1></button>
-                                    </div>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'isNull', columnName)} className='w-[150px] p-1'>
+                                            <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                                    <h1 >isNull</h1>
+                                            </div>
+                                            </button>
+                                        <button onClick={() => handleFilter(inputVariable,setInputVariable,'isNotNull', columnName)} className='w-[150px] p-1'>
+                                                <div className='hover:bg-[#dae7ff] p-1 rounded-sm cursor-pointer text-start'>
+                                                        <h1 >isNotNull</h1>
+                                                </div>
+                                        </button>
                                 </div>
   )
 }
