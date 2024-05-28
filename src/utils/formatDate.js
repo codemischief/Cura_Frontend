e41d16da8ccba1d42816652b65255ee0f3268278
Abhishek Ghanekar <a8hiyt@gmail.com>
2024-05-28 19:01:18 +0530

    updates

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,4 +1,5 @@
 export const formatDate = (dateStr) => {
+    if(dateStr == null || dateStr == "") return ""
     const dateObj = new Date(dateStr);
     const options = { day: '2-digit', month: 'short', year: 'numeric' };
     return new Intl.DateTimeFormat('en-GB', options).format(dateObj);
