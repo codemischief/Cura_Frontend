b6f325090b6163ebed2bb8d7254a896ef3467214
kapilsharma <erkapilsharma7@gmail.com>
2024-06-01 15:35:12 +0530

    changes in auth for session expire

--- src/context/routeMap.js
+++ src/context/routeMap.js
@@ -3,3 +3,11 @@ import { PATH_DASHBOARD } from "../route/path";
 export const routeMapObj = Object.freeze({
   BuilderInfo: PATH_DASHBOARD.admin.manageBuilder.root,
 });
+
+export const replaceKeys = (access_rights) => {
+  return Object.keys(access_rights).reduce((acc, key) => {
+    const newKey = routeMapObj[key] || key;
+    acc[newKey] = access_rights[key];
+    return acc;
+  }, {});
+};
