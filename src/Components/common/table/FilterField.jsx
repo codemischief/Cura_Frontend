1956c88987d5267cfd8f5ede1803742e1b11257f
kapilsharma <erkapilsharma7@gmail.com>
2024-06-05 12:05:14 +0530

    set min and max width of filter

--- src/Components/common/table/FilterField.jsx
+++ src/Components/common/table/FilterField.jsx
@@ -116,7 +116,7 @@ export const FilterField = (props) => {
   };
 
   return (
-    <div style={filterStyle}>
+    <div style={{ minWidth: "8rem", maxWidth: "8.5rem" }}>
       <div className="w-full  h-full flex justify-start py-3 px-1">
         <div className="w-full h-[1.75rem] flex justify-start items-center bg-[#F5F5F5] rounded-md">
           <input
